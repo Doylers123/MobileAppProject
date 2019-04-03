115f3484b6ea3c8a534978e48d36611f048e0693
CIAN DOYLE - Student <G00335783@gmit.ie>
2019-04-03 17:48:19 +0100

    First Commmit

---
+++ AnimeList/AnimeList/Model/Anime.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1.Model
+{
+    class Anime
+    {
+        #region == Public Properties ==
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Seasons { get; set; }
+        public string Episodes { get; set; }
+        public string Status { get; set; }
+        public string Rating { get; set; }
+
+        #endregion
+
+        #region  == constructors ==
+        public Anime()
+        {
+
+        }
+        public Anime(string t, string y, string s,
+                    string e, string st, string r)
+        {
+            Title = t;
+            Year = y;
+            Seasons = s;
+            Episodes = e;
+            Status = st;
+            Rating = r;
+        }
+
+        #endregion
+    }
+}
